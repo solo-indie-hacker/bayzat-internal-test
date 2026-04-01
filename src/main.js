017ffca2c20e3d85d0f12f8ad66cf36324eb42a4
Abdelrahman Emad <abdoemadselim11@gmail.com>
2026-04-01 16:29:01 +0200

    add env variables

--- src/main.js
+++ src/main.js
@@ -5,12 +5,12 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChang
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
-  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: "cs-material-test",
   storageBucket: "cs-material-test.firebasestorage.app",
   messagingSenderId: "398771475878",
-  appId: import.meta.env.FIREBASE_APP_ID
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
